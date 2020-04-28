40e3decd40245f0e03f1c344b541732a9d7fe2d8
sleepyarpan <arpanchattopadhyay23@gmail.com>
2020-04-28 20:09:02 +0530

    Connected backend using env and added base component to Home page using child

---
+++ src/core/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../styles.css';
+import Base from './Base';
+
+export default function Home() {
+  return (
+    <Base title='Home Page'>
+      <div className='row'>
+        <div className='col-4'>
+          <button className='btn btn-success'>Test</button>
+        </div>
+        <div className='col-4'>
+          <button className='btn btn-success'>Test</button>
+        </div>
+        <div className='col-4'>
+          <button className='btn btn-success'>Test</button>
+        </div>
+      </div>
+    </Base>
+  );
+}
